907c85d66a296e86f1fe9b5e7afa1b1912506070
devlara <createdevlara@gmail.com>
2024-07-16 19:06:07 +0900

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP 공부하기~~</h1>
+    <?php
+echo "안녕하세요!!";
+
+?>
+
+
+
+</body>
+</html>
