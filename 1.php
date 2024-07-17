488ac275117e218b41409a3eb844a7df6af25e76
devlara <createdevlara@gmail.com>
2024-07-17 21:51:04 +0900

    페이지 추가

---
+++ 1.php
@@ -0,0 +1,10 @@
+<?php
+// XAMPP
+// 웹서버, DB 서버, PHP
+// Apache, MariaDB, PHP 컴파일러
+// http://php.net/manual/en/install.php
+
+// 얘는 포트변경해서 나옴(전부 페이지 / 해서 설정해줘야지 확인가능함) : http://localhost/1.php
+
+$txt = "PHP";
+echo "I love $txt !";
