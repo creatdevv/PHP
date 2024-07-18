581911c115068b167e164233b004270d8b0c10c4
devlara <createdevlara@gmail.com>
2024-07-18 20:44:00 +0900

    변수와 같이 사용 가능!(계산됨)

--- 2.php
+++ 2.php
@@ -1,7 +1,15 @@
 <?php 
-// html 함께 사용 가능!
-echo "<h2>PHP는 재밌다</h2>";
-echo "Hello World! <br>";
-echo "I'm about to learn PHP!! <br>"; 
+# 변수와 같이 사용 가능(따라서 계산도 됨!)
+$txt1 = "Learn PHP";
+$x = 5;
+$y = 5;
+echo "<h2>.$txt1.</h2>";
+echo $x + $y
+
+/* >> 계산되어 나와서 아래처럼 출력됨.
+.Learn PHP. 
+        10 */
+
+
 
 ?>
