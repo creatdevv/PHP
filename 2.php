1c24019dff0156844b79b8d9fef5598da619f2b8
devlara <createdevlara@gmail.com>
2024-07-19 18:51:53 +0900

    echo str_word_count(한글일때 동작안함:0)

--- 2.php
+++ 2.php
@@ -1,9 +1,9 @@
 <?php 
 
 // strlen() 문자열 bytes 반환
+// $x_len = strlen("한글");
+// echo $x_len;  // 6 만 출력됨!(3바이트*2개=6)
 
-$x_len = strlen("한글");
-
-echo $x_len;  // 6 만 출력됨!(3바이트*2개=6)
+echo str_word_count("안녕 하세요 반갑 습니다");   // 0으로 출력됨 : 한글 동작 안함!
 
 ?>
