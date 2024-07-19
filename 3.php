66c8ddcddc73c51acbb6e3b4ba0eaf91fc95375b
devlara <createdevlara@gmail.com>
2024-07-19 20:59:53 +0900

    is_float(실수 판별)

--- 3.php
+++ 3.php
@@ -1,15 +1,16 @@
 <?php 
+// is_int(); 정수 판별
+// is_float(); 실수 판별
 
 $x = 33.8;
 
-if (is_int($x)){
-echo "x는 정수입니다.";
+if (is_float($x)){
+echo "x는 실수입니다.";
 
 } else {
-    echo "x는 정수가 아닙니다.";
+    echo "x는 실수가 아닙니다.";
 }
 
-// x는 정수가 아닙니다. 출력됨 (is_int / 33.8)
-
+// x는 실수입니다. 출력됨 (is_float / 33.8 실수니깐~)
 
 ?>
