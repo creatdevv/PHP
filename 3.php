a34fb6098977674b06c033f3a2f849bd27ad8db5
devlara <createdevlara@gmail.com>
2024-07-20 20:54:34 +0900

    띄어쓰기(\n) 부호

--- 3.php
+++ 3.php
@@ -6,6 +6,8 @@
     / 나누기,
     % 나머지,
     ** 제곱
+
+    띄어쓰기: \n;
 */
 
 $a = 5 + 4;
@@ -16,10 +18,16 @@ $e = 5 % 4;
 $f = 2 ** 3;            // 2의 3제곱이므로 8이 출력된다.
 
 echo $a;
+echo "\n";
 echo $b;
+echo "\n";
 echo $c;
+echo "\n";
 echo $d;
+echo "\n";
 echo $e;
+echo "\n";
 echo $f;
+
 
 ?>
