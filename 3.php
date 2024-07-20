6dbf554ea3a13fb4e623f29f2c4f82e732e24622
devlara <createdevlara@gmail.com>
2024-07-20 20:50:32 +0900

    php산술연산자(사칙연산, 나머지,제곱)

--- 3.php
+++ 3.php
@@ -1,24 +1,25 @@
-<script>
-    const GREETING = "1.안녕하세요."
-    document.write(GREETING)
-</script>
-<br>
 
 <?php 
-// abs() 절대값
-// sqrt()  루트
-// round() 반올림_가장 가까운 수로!!
-// rand()  난수_랜덤수(가로 안에 범위 지정 가능!)
-// $a = rand(10,12);
 
-define("GREETING", "2.안녕하세요");
+/*
+    PHP 산술 연산자
+    / 나누기,
+    % 나머지,
+    ** 제곱
+*/
 
+$a = 5 + 4;
+$b = 1 - 5;
+$c = 5 * 2;
+$d = 5 / 4;
+$e = 5 % 4;
+$f = 2 ** 3;
 
-// 1),2) 값을 대입하면 에러 발생!(상수는 변하지 않는 값이므로 또다른 값을 대입할 수 없다.)
-// GREETING = "DDD";            // 1) parce error(아예 실행 안함) 
-
-echo GREETING;
-
-// define("GREETING", "우하하");        // 2) notice error(큰 에러는 아니지만 에러 노티스 해줌)
+echo $a;
+echo $b;
+echo $c;
+echo $d;
+echo $e;
+echo $f;
 
 ?>
