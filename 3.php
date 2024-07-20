10609c8ff683e8f543fad98e1197edc7df8875b7
devlara <createdevlara@gmail.com>
2024-07-20 20:29:56 +0900

    round(반올림) 함수

--- 3.php
+++ 3.php
@@ -1,9 +1,15 @@
 <?php 
 // abs() 절대값
 // sqrt()  루트
+// round() 반올림_가장 가까운 수로!!
 
-$b =sqrt(4);
+$a = 0.75;
+$b = round($a);
+echo $b;          // 1 로 출력됨
 
-echo $b;            // 2가 출력됨
+
+$c = -2.75;
+$d =round($c);
+echo $d;         // -3 출력됨
 
 ?>
