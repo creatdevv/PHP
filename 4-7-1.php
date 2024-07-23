503748e48ed4a4ca16cd78adc3ae073fc4f4e7c6
devlara <createdevlara@gmail.com>
2024-07-23 23:04:54 +0900

    다차원배열 표 생성

---
+++ 4-7-1.php
@@ -0,0 +1,24 @@
+<!--표 양식-->
+
+<table border="1">
+    <tr>
+        <th>차종</th>
+        <th>재고량</th>
+        <th>판매량</th>
+    </tr>
+    <tr>
+        <th>볼보</th>
+        <th>22</th>
+        <th>10</th>
+    </tr>
+    <tr>
+        <th>산타페</th>
+        <th>25</th>
+        <th>4</th>
+    </tr>
+    <tr>
+        <th>아우디</th>
+        <th>12</th>
+        <th>11</th>
+    </tr>
+</table>
