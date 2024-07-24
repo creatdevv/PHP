25fe60148eac456a56e8162e07a55cf105af1b59
devlara <createdevlara@gmail.com>
2024-07-24 23:38:11 +0900

    수퍼전역($GLOBALS)

--- 5-1.php
+++ 5-1.php
@@ -1,14 +1,16 @@
 <?php 
-// #수퍼 전역
+// #수퍼 전역($Globals)
 
-$GLOBALS['aaa'] = "안녕";
+// $GLOBALS['aaa'] = "안녕";
 
 function aabb() {
-    echo $GLOBALS['aaa'];
+    $GLOBALS['aaa'] = "값!!!";
 }
 aabb();
 // >> 안녕 출력
 
+// echo $GLOBALS['aaa'];
+echo $aaa;
 
 
 
