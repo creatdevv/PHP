93058481ea5d4d6a1018a85e81fbcbaf6daa049e
devlara <createdevlara@gmail.com>
2024-07-31 21:44:59 +0900

    about,index1,menu 추가 / include, require 비교

---
+++ about.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php include 'menu.php'; ?>
+
+<p>어바웃 페이지입니다.</p>
+    
+</body>
+</html>
---
+++ index1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!-- <?php require 'menu1.php'; ?>       //  require 프로그램 중단 될 수 있어서 대부분은 include를 사용  --> 
+<?php include 'menu.php'; ?>
+<!-- <?php include_once 'menu.php'; ?>     // 한번만 삽입해야할 때 -->
+
+
+
+<p>인덱스 페이지입니다.</p>
+    
+</body>
+</html>
