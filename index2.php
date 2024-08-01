7d61a572b6ab1fb75c54d8c6248ccf1af515519c
devlara <createdevlara@gmail.com>
2024-08-01 22:02:33 +0900

    파일 업로드(file upload)

---
+++ index2.php
@@ -0,0 +1,21 @@
+
+<!-- //index.php_check_syntax
+
+    // upload.php_check_syntax -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>파일 업로드 샘플</title>
+</head>
+<body>
+    <form action="upload.php" enctype="multipart/form-data"> 
+        <input type="file" name="ufile">
+        <button>업로드</button>
+
+    </form>
+    
+</body>
+</html>
