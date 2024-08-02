9fa20374d4e7657b10954dd7a14555a7f340d34d
devlara <createdevlara@gmail.com>
2024-08-02 21:25:58 +0900

    create_file.php 접속시, 경로 안나오게 만들기

--- create_file.php
+++ create_file.php
@@ -4,10 +4,10 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $file_path = "/Applications/XAMPP/xamppfiles/htdocs/a.txt";
-$content = "이것은 a.txt 파일의 내용입니다.";
+$content = "Hello PHP";
 
 if (file_put_contents($file_path, $content) !== false) {
-    echo "파일이 성공적으로 생성되었습니다: $file_path";
+    echo "파일이 성공적으로 생성되었습니다. 내용: " . file_get_contents($file_path);
 } else {
     echo "파일 생성에 실패했습니다.";
 }
