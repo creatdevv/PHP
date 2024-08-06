e0e90b4854a80e7984b249f741eee9edc1500d09
devlara <createdevlara@gmail.com>
2024-08-06 23:01:31 +0900

    이미지업로드 방법들

--- dir.php
+++ dir.php
@@ -1,12 +1,19 @@
 <?php
-// 디렉토리(폴더) 파일 불러오기: dir() 함수 사용법
+// # 디렉토리(폴더) 파일 불러오기: dir() 함수 사용법
 
 $dir_name = "./upload";
 // if (is_dir($dir_name)) {
     $d = dir($dir_name);        // <생성>인스턴스 만들어 준다.
 
 while (( $file_name = $d ->read()) !== false) {
-    echo $file_name . "<br>";
+    if($file_name == '.'  or $file_name=='..'){
+        continue;
+    }
+    // echo $file_name . "<br>";
+    // echo '<img src="upload/'.$file_name.'" width="100">';        // 이미지 업로드(방법들)
+    // echo"<img src='upload/{$file_name}' width='100>";
+    ?><img src='upload/<?php  echo $file_name; ?>'width='100>
+    <?php
 }
 
     // echo "파일 목록:<br>";
