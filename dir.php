883fdc6cfdabee07c57069deff962dc20867e436
devlara <createdevlara@gmail.com>
2024-08-08 22:22:57 +0900

    filegaller(Directory_2)-파일업로드,이미지업로드

--- dir.php
+++ dir.php
@@ -1,44 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>File Gallery</title>
-</head>
-<body>
 <?php
-// # 디렉토리(폴더) 파일 불러오기: dir() 함수 사용법
+// 디렉토리(폴더) 파일 불러오기: dir() 함수 사용법
 
 $dir_name = "./upload";
-// if (is_dir($dir_name)) {
-    $d = dir($dir_name);        // <생성>인스턴스 만들어 준다.
+$d = dir($dir_name);        // <생성>인스턴스 만들어 준다.
 
-while (( $file_name = $d ->read()) !== false) {
-    if($file_name == '.'  or $file_name=='..'){
+while (($file_name = $d->read()) !== false) {
+    if($file_name == '.' or $file_name == '..'){
         continue;
     }
-
-    // echo $file_name . "<br>";
-
-    // 이미지를 HTML로 출력
-    // echo '<img src="upload/'.$file_name.'" width="100">';        // 이미지 업로드(3가지 방법)
-    // echo"<img src='upload/{$file_name}' width='100>";
-    ?>
-    <img src='upload/<?php echo $file_name; ?>' width='100'><br>
-    <?php
+    echo "<img src='upload/$file_name' width='100'><br>";
 }
 
-    // echo "파일 목록:<br>";
-    // while (($file_name = $d->read()) !== false) {    // <사용>
-    //     echo $file_name . "<br>";
-    // }
-
-    $d->close();                // <닫기>
-
-// } else {
-//     echo "디렉토리를 찾을 수 없습니다.";
-// }
-
-// http://localhost/file_gallery/dir.php
+$d->close(); // <닫기>
 ?>
-</body>
-</html>
+
