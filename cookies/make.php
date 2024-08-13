2b22fa47173c31bc8ed0868ca88f4eda498c1b0b
devlara <createdevlara@gmail.com>
2024-08-13 23:45:59 +0900

    초 변경

--- cookies/make.php
+++ cookies/make.php
@@ -1,11 +1,13 @@
 <?php 
 
 // *쿠키 만들기
-setcookie("ck_name", "홍길동", time() +240, "/");
-setcookie("ck_age", "100세", time() +240, "/");
-// ㄴ 설명: setcookie 함수를 이용함, ck_name 변수(쿠키이름), 들어갈 내용(이름), 유효기간설정- 얼마동안 사용할지 (끝나는시간-240초동안만), /로 경로설정 
+setcookie("ck_name", "홍길동", time() +5, "/");
+setcookie("ck_age", "100세", time() +10, "/");
+// ㄴ 설명: setcookie 함수를 이용함, ck_name 변수(쿠키이름), 들어갈 내용(이름), 유효기간설정- 얼마동안 사용할지 (끝나는시간 5초,10초동안만), /로 경로설정 
 // ㄴ 웹브라우저: localhost/cookies/cookie.php     >> 내 이름은 홍길동 입니다. 출력
 // $_COOKIE["ck_name]
+
+// 웹브라우저: localhost/cookies/make.php
 
 ?>
 쿠키가 생성되었습니다 <br>
