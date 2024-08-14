6f4b9bd73f4faf211a5002f495df66eda302d3e2
devlara <createdevlara@gmail.com>
2024-08-14 23:06:03 +0900

    session (1.php에 $COOKIE 주석- 세션 아이디 부여해서 키 생성후 사용)

--- session/1.php
+++ session/1.php
@@ -6,6 +6,11 @@ session_start();
 $_SESSION['ss_name']  = '홍길동';
 $_SESSION['s_age']  = '14세'
 
+
+// echo $_COOKIE['PHPSESSID']; 
+// 세션에 아이디 부여해서 키로 사용할 수 있게 서버에 생성됨 (나머지 다 주석처리하고 이것만 해서 확인해보자!)
+
+
 ?>
 세션이 생성되었습니다.
 <a href="2.php">세션확인</a>
