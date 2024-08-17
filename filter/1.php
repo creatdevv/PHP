e59c58ff5568942d8419f260ad69ce5bc9f3e455
devlara <createdevlara@gmail.com>
2024-08-17 18:18:11 +0900

    정수, 실수 필터 확인

--- filter/1.php
+++ filter/1.php
@@ -1,14 +1,23 @@
 <?php 
 
+// # 정수 필터: FILTER_VALIDATE_INT
+// # 실수 필터: FILTER_VALIDATE_FLOAT)
+
+
 // $i = 100;
 // $i = 'a';        // 아무것도 출력 안됨
-$i = '33.333';
+// $i = '33.333';         // 정수필터 x
+//  $i = 'AJJJ-0-0';     // 실수필터 x
+ $i = 0;
 
-$j = filter_var($i, FILTER_VALIDATE_INT);
+// $j = filter_var($i, FILTER_VALIDATE_INT);
+$j = filter_var($i, FILTER_VALIDATE_FLOAT);
 
 // echo $j;            // 검증 확인완료(웹브라우저에서 100 출력됨- filter/1.php) / $I = 100; 일 경우에만 적용!
 
-if($j) {
+
+// if($j) {
+if(filter_var($i, FILTER_VALIDATE_INT) === 0 || !filter_var($i, FILTER_VALIDATE_INT) == false) {
     echo $i. "는 제대로 된 정수입니다,.";
 } else {
     echo $i. "는 제대로 된 정수가 아닙니다.";
