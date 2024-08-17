010a784fac3094147d28f39fbcee6d30aa72afee
devlara <createdevlara@gmail.com>
2024-08-17 17:51:54 +0900

    회원전용페이지 수정 및 기타 설명들 내용추가

--- login/login_ok.php
+++ login/login_ok.php
@@ -24,20 +24,20 @@ if($pw == '') {
     exit;
 }
 
-if($id == 'guest' && $pw == '1234'){       // id와 pw의 예시 가정
-    session_start();
+if($id == 'guest' && $pw == '1234'){       // id와 pw의 가정(정보확정)- 나중에 데이터베이스로 사용시, (아이디,비번을) db에서 쿼리 체크해서!
+    session_start();                // 세션 시작 설정해주고~~
 
     $_SESSION['ss_id'] = $id;
 
     echo "<script>
-        alert('로그인 성공했습니다.');
-        self.location.href='member.php';    // 회원전용 페이지로 이동
+        alert('로그인 성공했습니다.');          // 1)로그인 성공시
+        self.location.href='member.php';    // *회원전용 페이지로 이동
     </script>"; 
 
 } else {
 
     echo "<script>
-    alert('로그인 실패했습니다. 아이디와 비번을 확인해 주세요.');
+    alert('로그인 실패했습니다. 아이디와 비번을 확인해 주세요.');   // 2)둘 중 하나 잘못입력시
     self.location.href='index.php';  
 </script>";
 
--- login/member.php
+++ login/member.php
@@ -1,12 +1,13 @@
 <?php 
 session_start();
 
-if(isset($_SESSION['ss_id']) AND $_SESSION['ss_id'] != '')  // session id가 세팅이 되어있거나(true), 비어있거나)
- {
+if( !isset($_SESSION['ss_id']) or $_SESSION['ss_id'] == '')  
+// session id가 세팅이 되어있거나(true), 비어있거나(false)
+
+{
     echo "
     <script>
     alert('여기는 회원 전용 페이지 입니다. 로그인 후 접근이 가능합니다.');
-    history.go(-1)
     self.location.href='index.php';             //로그인페이지로 보내기      
     </script>
     ";
