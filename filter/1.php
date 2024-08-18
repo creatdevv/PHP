88d5cec53507e925e11913103dbc191846d9643b
devlara <createdevlara@gmail.com>
2024-08-18 17:42:35 +0900

    입력값 검사(ip)

--- filter/1.php
+++ filter/1.php
@@ -8,19 +8,29 @@
 // $i = 'a';        // 아무것도 출력 안됨
 // $i = '33.333';         // 정수필터 x
 //  $i = 'AJJJ-0-0';     // 실수필터 x
- $i = 0;
+//  $i = 0;
+// $ip = '127.0.0.1';
+$ip = '127.0. 0.12';
+
 
 // $j = filter_var($i, FILTER_VALIDATE_INT);
-$j = filter_var($i, FILTER_VALIDATE_FLOAT);
+// $j = filter_var($i, FILTER_VALIDATE_FLOAT);
+$j = filter_var($ip, FILTER_VALIDATE_IP);
 
 // echo $j;            // 검증 확인완료(웹브라우저에서 100 출력됨- filter/1.php) / $I = 100; 일 경우에만 적용!
 
 
 // if($j) {
-if(filter_var($i, FILTER_VALIDATE_INT) === 0 || !filter_var($i, FILTER_VALIDATE_INT) == false) {
-    echo $i. "는 제대로 된 정수입니다,.";
+// if(filter_var($i, FILTER_VALIDATE_INT) === 0 || !filter_var($i, FILTER_VALIDATE_INT) == false) {
+if($j) {
+
+    // echo $i. "는 제대로 된 정수입니다.";
+    echo $ip. "는 제대로 된 ip입니다.";
+
 } else {
-    echo $i. "는 제대로 된 정수가 아닙니다.";
+    // echo $i. "는 제대로 된 정수가 아닙니다.";
+    echo $ip. "는 제대로 된 ip가 아닙니다.";
+
 }
 
 ?>
