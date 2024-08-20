6f1df299373b9d139e3e32a33d2f2338a2cf95e4
devlara <createdevlara@gmail.com>
2024-08-20 23:08:14 +0900

    callback함수(내장함수 사용)

---
+++ callback/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+function my_callback($item) {
+    return strlen($item);
+}
+
+$strings = ["apple", "orange", "pear", "pineapple"];
+
+$lengths = array_map("my_callback", $strings);  
+// array_map 함수를 사용하면, ()안에 있는 것을 사용하는데 위에 있는[]배열의 횟수 만큼 호출해서 담아주는 것이다.
+// 1) 사용자 내장함수(array_map))에서 사용하는 콜백함수 알아봄 : 글자수 대로 출력되므로 [0~3] => 5,6,4,9 차례로 출력됨
+
+print_r($lengths);
+
+?>
