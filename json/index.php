41b3380e137d7933c4c02a50c7cf8896049fc29d
devlara <createdevlara@gmail.com>
2024-08-21 12:07:21 +0900

    array / object 타입으로 정렬

--- json/index.php
+++ json/index.php
@@ -8,13 +8,24 @@ $age = [
     ];
 
 //PHP 연관배열 => JSON
-// echo json_encode($age);
+// echo json_encode($age);      // Peter~Joe 차례로 출력
 
-$json = '{"Peter":35, "Ben":37, "Joe":20 }';
+
 
 // var_dump(json_decode($json));
-$arr = json_decode($json);
+// $arr = json_decode($json);
 
-echo $arr->Peter;           //>> 35 출력
+// $json = '{"Peter":35, "Ben":37, "Joe":20 }';
+// $obj = json_decode($json, false);
+// echo $arr->Peter;           
+
+$json = '{"Peter":35, "Ben":37, "Joe":20 }';
+$obj = json_decode($json, true);
+// $arr = json_decode($json, true);         // >> NULL 출력
+
+var_dump($obj);      
+// array 형태로 출력됨 >> array(3) { ["Peter"]=> int(35) ["Ben"]=> int(37) ["Joe"]=> int(20) } 출력
+
+
 
 ?>
