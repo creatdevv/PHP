77325babdf3a4748bc1316f93742277f8d3766c8
devlara <createdevlara@gmail.com>
2024-08-26 15:22:10 +0900

    db연결 2번째방법

--- db/index.php
+++ db/index.php
@@ -4,11 +4,14 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 
-$conn = new mysqli($servername, $username, $password);
+// $conn = new mysqli($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password);
 
-if ($conn->connect_error) {
-    echo "DB 연결에 실패했습니다.";
-    exit;
+// if ($conn->connect_error) {
+    // echo "DB 연결에 실패했습니다.";
+    // exit;
+if (!$conn) {
+    die("DB 연결에 실패했습니다.");
 }
 
 echo "DB 연결에 성공했습니다.";
