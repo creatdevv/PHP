f55513c9da9a278011b302c58da513415cb2b5b7
devlara <createdevlara@gmail.com>
2024-08-27 12:01:25 +0900

    firstdb1 삭제

--- db/createdb.php
+++ db/createdb.php
@@ -15,8 +15,8 @@ try {
 
 try {
 //  $sql = "CREATE DATABASE firstdb";       // phpMyAdmin에 firstdb 만들어 줄 것임
- $sql = "CREATE DATABASE firstdb1";       
-//  $sql = "DROP DATABASE firstdb1";      
+//  $sql = "CREATE DATABASE firstdb1";       
+ $sql = "DROP DATABASE firstdb1";      
  $conn->exec($sql);                     // *DB 만들것을 실행 명령어(근데 실행 안될 수도 있으므로 또 try&catch문으로 잡아주기)
 
 echo "<p>firstdb가 생성되었습니다.</p>";    // 정상적으로 작동시, try문 실행됨  
