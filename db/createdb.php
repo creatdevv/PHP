b01b2e210cbb41da19bdded019cf4791e54f8f88
devlara <createdevlara@gmail.com>
2024-08-27 12:00:44 +0900

    firstdb1도 생성

--- db/createdb.php
+++ db/createdb.php
@@ -14,10 +14,14 @@ try {
 }
 
 try {
- $sql = "CREATE DATABASE firstdb";       // phpMyAdmin에 firstdb 만들어 줄 것임
- $conn->exec($sql);
- echo "<p>firstdb가 생성되었습니다.</p>";                      // DB 만들것을 실행하기(근데 실행 안될 수도 있으므로 또 try&catch문으로 잡아주기)
-}catch(PDOException $e) {
+//  $sql = "CREATE DATABASE firstdb";       // phpMyAdmin에 firstdb 만들어 줄 것임
+ $sql = "CREATE DATABASE firstdb1";       
+//  $sql = "DROP DATABASE firstdb1";      
+ $conn->exec($sql);                     // *DB 만들것을 실행 명령어(근데 실행 안될 수도 있으므로 또 try&catch문으로 잡아주기)
+
+echo "<p>firstdb가 생성되었습니다.</p>";    // 정상적으로 작동시, try문 실행됨  
+
+}catch(PDOException $e) {               // 에러발생시, catch문 실행됨
     echo $e->getMessage();
 }
 
