9825efd2746a6384b734d9918df0ce63c071ac33
devlara <createdevlara@gmail.com>
2024-09-07 21:14:45 +0900

    fetch_assoc/num/both(default)

--- db/list.php
+++ db/list.php
@@ -4,9 +4,13 @@ include 'db.php';
 $sql = "SELECT * FROM myguests";
 $stmtt = $conn->prepare($sql);
 $stmt->execute();           // 실행
-$rs = $stmt->fetchAll();    // 가져오기
+// $rs = $stmt->fetchAll();    // 가져오기
+$rs = $stmt->fetchAll(PDO::FETCH_ASSOC);    // 필드명 기준으로 가져오기
+$rs = $stmt->fetchAll(PDO::FETCH_NUM);    // [ ]번호 순서대로 가져오기
+$rs = $stmt->fetchAll(PDO::FETCH_BOTH);    // 디폴트 기준값 으로 가져오기
 
-var_dump($rs);
+// var_dump($rs);
+print_r($rs);
 
 
 ?>
