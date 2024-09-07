3adc0e0b2242c36aa82c1935fe42c7183033ed51
devlara <createdevlara@gmail.com>
2024-09-07 21:17:18 +0900

    table형성

--- db/list.php
+++ db/list.php
@@ -10,7 +10,16 @@ $rs = $stmt->fetchAll(PDO::FETCH_NUM);    // [ ]번호 순서대로 가져오기
 $rs = $stmt->fetchAll(PDO::FETCH_BOTH);    // 디폴트 기준값 으로 가져오기
 
 // var_dump($rs);
-print_r($rs);
+// print_r($rs);
+echo "<table border='1'>";
+
+foreach($rs AS $row) {
+    echo "<tr>
+    <td>".$row['firstname']."</td>
+    </tr>";
+}
+
+echo "</table>";
 
 
 ?>
