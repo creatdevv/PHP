47a53734c479463e81492a8228b1c802d03fe715
devlara <createdevlara@gmail.com>
2024-09-09 18:27:01 +0900

    개행표현: nl2br

--- IMSI/view.php
+++ IMSI/view.php
@@ -3,13 +3,11 @@ include 'db.php';
 
 $idx = $_GET["idx"];
 
-$sql = "SELECT * FROM board WHERE idx={$idx}";
+$sql = "SELECT * FROM board WHERE idx = :idx";
 $stmt = $conn->prepare($sql);
+$stmt->bindParam(':idx', $idx, PDO::PARAM_INT);
 $stmt->execute();
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
-$row = $stmt->fetch(PDO::FETCH_BOTH);       // Default 값
-$row = $stmt->fetch(PDO::FETCH_NUM);       // 숫자로 보여주기
-
 
 // #pre : 나와있는 함수 그대로 출력하겠다.
 // echo "<pre>";
@@ -23,10 +21,10 @@ $row = $stmt->fetch(PDO::FETCH_NUM);       // 숫자로 보여주기
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $row['subject']; ?></title>
+    <title><?php echo htmlspecialchars($row['subject']); ?></title>
 </head>
 <body>
-    <p>제목 : <?php echo $row['subject']; ?></p>
-    <p><?php echo $row['content']; ?></p>
+    <p>제목 : <?php echo htmlspecialchars($row['subject']); ?></p>
+    <p><?php echo nl2br(htmlspecialchars($row['content'])); ?></p>
 </body>
 </html>
