9466b17894542b856fadd7ea9746c1702f6c4171
devlara <createdevlara@gmail.com>
2024-09-09 18:25:01 +0900

    글내용 한줄씩 표현됨

--- IMSI/view.php
+++ IMSI/view.php
@@ -11,10 +11,22 @@ $row = $stmt->fetch(PDO::FETCH_BOTH);       // Default 값
 $row = $stmt->fetch(PDO::FETCH_NUM);       // 숫자로 보여주기
 
 
-// pre : 나와있는 함수 그대로 출력하겠다.
-echo "<pre>";
-print_r($row);
-echo "</pre>";
-
-
+// #pre : 나와있는 함수 그대로 출력하겠다.
+// echo "<pre>";
+// print_r($row);
+// echo "</pre>";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $row['subject']; ?></title>
+</head>
+<body>
+    <p>제목 : <?php echo $row['subject']; ?></p>
+    <p><?php echo $row['content']; ?></p>
+</body>
+</html>
