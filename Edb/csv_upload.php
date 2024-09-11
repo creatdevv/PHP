0ca44b13d7561e790e413a15f8470c2476b29b89
devlara <createdevlara@gmail.com>
2024-09-11 20:49:12 +0900

    conn으로 sql 실행

--- Edb/csv_upload.php
+++ Edb/csv_upload.php
@@ -14,16 +14,19 @@ while(($line = fgetcsv($file)) !== FALSE) {
     $sql = "INSERT INTO csvmember(cs_name, cs_email) VALUES('". $line[0]."',
     '". $line[1] ."');";
 
-    echo $sql;
-    exit;
+    // echo $sql;
+    // exit;
+
+    $conn->exec($sql);      // db에서 conn 의 이름으로 연결했었기 때문이고(setAttribute~), 다른 이름으로 바꿔서 할 수도 있다.
 
 }
 
 fclose($file);      // 열었으면, 반드시 닫아주기
 
-print_r($arr);      // 출력문으로 확인해보기 
+// print_r($arr);      // *출력문으로 확인해보기 
 // >> member.php에 있는 목록들 배열로 0~4 나오고, 세부정보도 다시 0,1로 해서 나눠져서 순서대로 출력됨 확인됨
 
+$conn = null;    // 한번 실행 끝나면, 초기화 해줘야하니깐~~
 
 
 ?>
