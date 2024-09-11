16fdb15a16e0d0361f47ab25a12de1ee4f18b4e5
devlara <createdevlara@gmail.com>
2024-09-11 20:53:27 +0900

    beginTransaction > commit : 데이터 많을때 한꺼번에 한번에 밀어넣기

--- Edb/csv_upload.php
+++ Edb/csv_upload.php
@@ -8,6 +8,8 @@ print_r($_FILES);
 // $arr = [];      //배열
 
 $file = fopen($_FILES['csv']['tmp_name'], 'r');
+
+$conn->beginTransaction();      //-데이터 많을때, 한꺼번에 한번에 밀어넣기!
 while(($line = fgetcsv($file)) !== FALSE) {
     // array_push($arr, $line);
 
@@ -20,6 +22,8 @@ while(($line = fgetcsv($file)) !== FALSE) {
     $conn->exec($sql);      // db에서 conn 의 이름으로 연결했었기 때문이고(setAttribute~), 다른 이름으로 바꿔서 할 수도 있다.
 
 }
+
+$conn->commit();        //-데이터많을떄, 한꺼번에 밀어넣기!
 
 fclose($file);      // 열었으면, 반드시 닫아주기
 
