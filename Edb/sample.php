aba7813c6776b441fa63d9f0f0e1f4539db1ce0a
devlara <createdevlara@gmail.com>
2024-09-13 23:17:03 +0900

    short_open_tag 알아보기

---
+++ Edb/sample.php
@@ -0,0 +1,17 @@
+<?php echo "PHP 문서입니다.";
+
+phpinfo();          // short_open_tag 찾아보기(메모장 열어서 복/붙하고 찾기) >> 설정 완료하면, 꺽세 안보임!^^
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?= "php문서입니다."; ?>
+</body>
+</html>
