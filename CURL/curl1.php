954726c7d4425a1876d24b365fefcd5a599b1640
devlara <createdevlara@gmail.com>
2024-09-14 16:02:37 +0900

    타사이트 방문 가져오기

--- CURL/curl1.php
+++ CURL/curl1.php
@@ -5,8 +5,9 @@ echo "curl sample <br>";
 $ch = curl_init();
 
 // # URL 설정
-// curl_setopt($ch, CURLOPT_URL, 'http://cbtti.or.kr');     //http 통신을 통해 한 것임
-curl_setopt($ch, CURLOPT_URL, 'https://phpschool.com/');     //다른사이트(한글꺠져 나옴). 하단 한글 깨지지 않게 변경!
+// curl_setopt($ch, CURLOPT_URL, 'http://cbtti.or.kr');     //1-사이트: http 통신을 통해 한 것임
+// curl_setopt($ch, CURLOPT_URL, 'https://phpschool.com/');   //2-사이트: 한글꺠져 나옴, 하단 한글 깨지지 않게 변경!
+curl_setopt($ch, CURLOPT_URL, 'https://www.daum.net/');   // 3-사이트(얘는 같은 거라 그냥 response로 출력하기)
 
 // 응답을 문자열로 반환받기
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -21,15 +22,14 @@ if ($response === false) {
     // curl 오류 출력
     echo 'Curl error: ' . curl_error($ch);
 } else {
-    // 문자 인코딩 자동 감지 및 변환(해당사이트의 소스코드 확인후, 헤드밑의 이부분 찾아서 변환하기)
-    $response = mb_convert_encoding($response, 'UTF-8', 'EUC-KR');
-    echo $response;
+    // #문자 인코딩 자동 감지 및 변환(해당사이트의 소스코드 확인후, 헤드밑의 이부분 찾아서 변환하기)
+    // $response = mb_convert_encoding($response, 'UTF-8', 'EUC-KR'); // 1,2 사이트
+    $response = mb_convert_encoding($response, 'UTF-8', 'auto');      // 3 사이트
+    echo $response;            
+
 }
 
 curl_close($ch);
-
-
-
-// echo$response;
+ 
 
 ?>
