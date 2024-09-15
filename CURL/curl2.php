d6828b2d9f5cba7b633235b1cbcd552579a646c0
devlara <createdevlara@gmail.com>
2024-09-15 15:00:52 +0900

    print_r($arr)로 객체 전체를 출력하는 대신, 필요한 값만 출력하기

--- CURL/curl2.php
+++ CURL/curl2.php
@@ -6,15 +6,15 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'http://echo.jsontest.com/temperature/-9.3/humidity/0.40/wind/3');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-$resoponse = curl_exec($ch);
+$response = curl_exec($ch);
 curl_close($ch);
-// echo $resoponse;
 
 // #데이터 배열로 바꾸기
-$arr = json_decode($resoponse);
+$arr = json_decode($response);
 
-print_r($arr);
-echo $arr->wind;
-
+// 필요한 데이터만 출력
+echo "Temperature: " . $arr->temperature . "<br>";
+echo "Humidity: " . $arr->humidity . "<br>";
+echo "Wind: " . $arr->wind . "<br>";
 
 ?>
