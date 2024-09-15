7843b262edad6e43163c551e21980b9041c45cde
devlara <createdevlara@gmail.com>
2024-09-15 15:07:15 +0900

    표(table)만들기

--- CURL/curl2.php
+++ CURL/curl2.php
@@ -12,14 +12,30 @@ curl_close($ch);
 // #데이터 배열로 바꾸기
 $arr = json_decode($response);
 
-// // 필요한 데이터만 출력
+//1) 필요한 데이터만 출력
 // echo "Temperature: " . $arr->temperature . "<br>";
 // echo "Humidity: " . $arr->humidity . "<br>";
 // echo "Wind: " . $arr->wind . "<br>";
 
-foreach($arr AS $key => $var) {
-    echo $key .':'. $var;
-    echo "<br>";
-}
+// 2) 필요한 데이터만 출력(짧은식)
+// foreach($arr AS $key => $var) {
+//     echo $key .':'. $var;
+//     echo "<br>";
+// }
 
 ?>
+<table border="1">
+<tr>
+    <td>온도</td>
+    <td><?= $arr->temperature; ?></td>
+</tr>
+<tr>
+    <td>습도</td>
+    <td><?= $arr->humidity; ?></td>
+</tr>
+<tr>
+    <td>풍속</td>
+    <td><?= $arr->wind; ?>m/s</td>
+</tr>
+
+</table>
