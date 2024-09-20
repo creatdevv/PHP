8991f6d9a43864e0eec282c0b0ea6bbec1bfb8e0
devlara <createdevlara@gmail.com>
2024-09-20 23:09:52 +0900

    추가

--- AJAX/check_id.php
+++ AJAX/check_id.php
@@ -18,6 +18,8 @@ $arr = array("result" => $row["cnt"] ? "exist" : "not_exist");
 // $rs = $row["cnt"] ? "exist" : "not_exist";       >> 위와 같은 방식
 // $arr = array("result"=> $rs);
 
+die(json_encode($arr));             // 프로그램 끝을 알리는 것(아래와 같다, 이상으로 밑에 뭘 작성해도 안됨, 여기가 끝)
+// exit(json_encode($arr));            
 
 
 ?>
