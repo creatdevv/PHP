36df27a60b475d674fa314bbee39b76172e3b0cf
devlara <createdevlara@gmail.com>
2024-09-20 23:05:09 +0900

    쿼리문 작성

--- AJAX/check_id.php
+++ AJAX/check_id.php
@@ -3,8 +3,21 @@
 require_once("db.php");
 
 $user_id = $_GET["id"];
+// echo $user_id;
 
-echo $user_id;
+$sql = "SELECT COUNT(*) cnt FROM member WHERE user_id". $user_id ."'";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+
+$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+$roow = $stmt->fetch();
+
+// print_r($row);       >>확인용
+
+$arr = array("result" => $row["cnt"] ? "exist" : "not_exist");
+
+
+
 
 ?>
 
--- AJAX/db.php
+++ AJAX/db.php
@@ -9,7 +9,7 @@ try {
 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-echo "<p>DB 연결에 성공했습니다.</p>";
+// echo "<p>DB 연결에 성공했습니다.</p>";
 
 } catch(PDOException $e) {
     echo $e->getMessage();
