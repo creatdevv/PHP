eb52cbb633db2fefeb1adadd48c3908c091f3d29
devlara <createdevlara@gmail.com>
2024-09-23 14:37:57 +0900

    multiple 파일 여러장 업로드일때

--- AJAXFILEUPLOAD/upload/ajax.php
+++ AJAXFILEUPLOAD/upload/ajax.php
@@ -1,13 +1,34 @@
 <?php 
 
 // print_r($FILES["photo"]);
-// print_r($FILES);
+// print_r($FILES);                 // 결과 확인
 
-copy($_FILES['photo']['tmp_name'], "upload/" . $_FILES['photo']['name']);
+// #파일 1개 업로드 넘어오는거 받기
+// copy($_FILES['photo']['tmp_name'], "upload/" . $_FILES['photo']['name']);
+// $arr = array("resulr" => "success", "img" => "upload/". $_FILES['photo']['name']);
+// die(json_encode($arr));
 
+// #파일 여러개 업로드 넘어보낼때
+if(is_array($_FILES['photo']['tmp_name'])) {
 
-$arr = array("resulr" => "success", "img" => "upload/". $_FILES['photo']['name']);
+    $cnt = count($_FILES['photo']['name']);
+    $rs_arr = [];               // = $rs_arr = arrray; (배열로 만든이유는 이미지를 한꺼번에 여러개 넘겨주기 위해서)
+    for ($i = 0; $i < $cnt; $i++) {
+        copy($_FILES['photo']['tmp_name'][$i], "upload/" .$_FILES['photo']['name']);
+        $rs_arr[] = "upload/". $_FILES['photo']['name'][$i];
+    }
 
-die(json_encode($arr));
+    $arr = array("result" => "success", "img" => implode('|', $rs_arr));
+
+    // upload/1.png|upload/2.png
+
+    die(json_encode($arr));             // = {"result": "success", "img" : "upload/1.png|upload/2.png"}   >>개발자도구에 이런식으로 결과 생성될 것임
+    
+
+} else {
+    copy($_FILES['photo']['tmp_name'], "upload/" . $_FILES['photo']['name']);
+    $arr = array("result" => "success", "img" => "upload/". $_FILES['photo']['name']);
+    die(json_encode($arr));
+}
 
 ?>
