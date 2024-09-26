f2d956b31eff83b0e577921e3f3d85b487992648
devlara <createdevlara@gmail.com>
2024-09-26 11:05:51 +0900

    출력본 기록(0,1~)

--- AJAX/ajax_poll.php
+++ AJAX/ajax_poll.php
@@ -17,7 +17,16 @@ if(file_exists($filename)) {
 $content = file_get_contents($filename);            // 위의 0,0이 이 변수에 담아질 것~~
 list($yes, $no) = explode(',', $content);           // txt에 미리 값 생성(2개)하면 밑의 결과값에 따라 숫자 보임!
 
-echo 'yes'. $yes ."<br>";
-echo 'no'. $no ."<br>";
+// echo 'yes'. $yes ."<br>";
+// echo 'no'. $no ."<br>";
+
+if($vote == 0 ) {
+    $yes = $yes +1;
+} elseif($vote == 1 ) {
+    $no = $no +1;
+}
+
+file_get_contents($filename, "$yes,$no");           // 찍힌걸 기록을 해줌(계속 축적됨) 
+echo $yes . ' '. $no;
 
 ?>
