2d23a2d02a5c2f2eb2ef2e6f656cd898a06ca6af
devlara <createdevlara@gmail.com>
2024-09-29 20:02:29 +0900

    로그인의 아이디,비번 자리를 바인딩 해주기

--- SQLINJECTION/login.php
+++ SQLINJECTION/login.php
@@ -5,7 +5,18 @@ include 'db.php';
 $id = $_POST['id'];
 $pw = $_POST['pw'];
 
-$sql = "SELECT * FROM member WHERE user_id='{$id}' AND passwd='{$pw}'";
-echo $sql;
+// $sql = "SELECT * FROM member WHERE user_id='{$id}' AND passwd='{$pw}'";
+$sql = "SELECT * FROM member WHERE user_id=:user_id AND passwd=:pw";
+// echo $sql;
+$stmt = $conn->prepare($sql);
+
+// 바인딩 해주기
+$stmt->bindParam(':user_id', $id);
+$stmt->bindParam(':pw', $pw);
+
+$stmt->execute();
+$row = $stmt->fetch();
+
+var_dump($row);
 
 ?>
