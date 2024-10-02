b670c6ab6cd9517d035ac0500bce90ac0c19576a
devlara <createdevlara@gmail.com>
2024-10-02 19:28:58 +0900

    로그인 정보 넘기기(성공,실패-f12확인)

--- bootstrap/login_ok.php
+++ bootstrap/login_ok.php
@@ -2,7 +2,28 @@
 
 include "db.php";
 
-print_r($_POST);        // 로그인시 정보 넘어오는지 확인~~
+// print_r($_POST);        // 로그인시 정보 넘어오는지 확인~~
 
+$id = $_POST['id'];
+$pwd = $_POST['pwd'];
+
+$sql = "SELECT * FROM member WHERE user_id=:user_id AND passwd=pw";
+$stmt = $conn->prepare($sql);
+$stmt->bindParam(':user_id', $id);
+$stmt->bindParam(':pw', $pw);
+$stmt->execute();
+$row = $stmt->fetch();
+
+if($row) {
+    session_start();
+    $_SESSION['id'] = $id;
+
+    $arr = ['result' => 'success'];
+    die(json_encode($arr));
+
+} else {
+    $arr = ['result' => 'fail'];
+    die(json_encode($arr));
+}
 
 ?>
