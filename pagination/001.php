5439d96026588a0789ccad4d4cc47c3ee1207b7a
devlara <createdevlara@gmail.com>
2024-10-08 23:15:42 +0900

    디자인 없는 순수 페이징 처리 소스

--- pagination/001.php
+++ pagination/001.php
@@ -68,6 +68,52 @@ echo "<a href='001.php?page=". $total_page."'>Last</a>";
 // 11 11~15
 // ... 총 페이지 수 까지
 
+$start_page = ( ( floor( ($page - 1 ) / $page_limit ) ) * $page_limit ) + 1;
+$end_page = $start_page + $page_limit -1;
 
+if($end_page > $total_page) {
+  $end_page = $total_page;
+}
+
+$prev_page = $start_page - 1;
+if($prev_page < 1) {
+  $prev_page = 1;
+}
+
+// 1page 0.0 --> 0*5 + 1 = 1
+// 2page 0.2 --> 0*5 + 1 = 1
+// 3page 0.4 --> 0*5 + 1 = 1
+// 4page 0.6 --> 0*5 + 1 = 1
+// 5page 0.8 --> 0*5 + 1 = 1
+// 6page 1.0 --> 1*5 + 1 = 6
+// 7page 1.2 --> 1*5 + 1 = 6
+// 8page 1.4 --> 1*5 + 1 = 6
+// 9page 1.6 --> 1*5 + 1 = 6
+// 10page 1.8 --> 1*5 + 1 = 6
+// 11page 2.0 --> 1*5 + 1 = 11
+
+echo "<a href='001.php?page=1'>First</a> ";
+
+if($prev_page > 1) {
+  echo "<a href='001.php?page={$prev_page}'>Prev</a> ";
+}
+
+for($i = $start_page; $i <= $end_page; $i++) {
+  if($i == $page) {
+    echo $i .' ';
+  }else {
+    echo "<a href='001.php?page={$i}'>{$i}</a> ";
+  }
+}
+
+$next_page = $end_page + 1;
+if($next_page <= $total_page) {
+  echo "<a href='001.php?page={$next_page}'>Next</a> ";
+}
+
+if($page < $total_page) {
+  echo "<a href='001.php?page={$total_page}'>Last</a> ";
+
+}
 
 ?>
