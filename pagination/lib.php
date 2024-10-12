8f2461c659e0d0bdda163a0440189093b323da2a
devlara <createdevlara@gmail.com>
2024-10-12 23:21:49 +0900

    코드 수정

--- pagination/lib.php
+++ pagination/lib.php
@@ -1,20 +1,48 @@
-<?php 
-$total = 101;
-$limit = 10;
-$page_limit = 5;
-$page = (isset($_GET['page'])&& $_GET['page'] != '' && is_numeric($_GET['page'])) ? $_GET['page'] : 1;
+<?php
 
-/*
- total : 게시물의 총 개수
- limit : 게시물의 총 개수
- page_limit : 출력페이지수
- page : 현재페이지
-*/
+function my_pagination($total, $limit, $page_limit, $page, $base_url) {
+    $total_page = ceil($total / $limit);
+    $start_page = ((floor(($page - 1) / $page_limit)) * $page_limit) + 1;
+    $end_page = $start_page + $page_limit - 1;
 
-$rs_str = my_pagination($total, $limit, $page_limit, $page);
+    if ($end_page > $total_page) {
+        $end_page = $total_page;
+    }
 
-echo $rs_str;
+    $pagination_str = "";
 
+    // First 링크
+    if ($page > 1) {
+        $pagination_str .= "<a href='{$base_url}?page=1'>First</a> ";
+    }
 
+    // Prev 링크
+    $prev_page = $page - 1;
+    if ($prev_page >= 1) {
+        $pagination_str .= "<a href='{$base_url}?page={$prev_page}'>Prev</a> ";
+    }
+
+    // 페이지 번호 출력
+    for ($i = $start_page; $i <= $end_page; $i++) {
+        if ($i == $page) {
+            $pagination_str .= "<strong>{$i}</strong> ";
+        } else {
+            $pagination_str .= "<a href='{$base_url}?page={$i}'>{$i}</a> ";
+        }
+    }
+
+    // Next 링크
+    $next_page = $page + 1;
+    if ($next_page <= $total_page) {
+        $pagination_str .= "<a href='{$base_url}?page={$next_page}'>Next</a> ";
+    }
+
+    // Last 링크
+    if ($page < $total_page) {
+        $pagination_str .= "<a href='{$base_url}?page={$total_page}'>Last</a> ";
+    }
+
+    return $pagination_str;
+}
 
 ?>
--- todolist/todolist_ok.php
+++ todolist/todolist_ok.php
@@ -1,105 +1,26 @@
-<?php 
+<?php
 
-// print_r($_POST);   // post 방식으로 넘겼을때 주소값 표시 안되는지 결과값 확인 >> 정상적으로 넘어가고 페이지 내용에 표시 확인완료(제대로 받아진 것 확인!)
+include 'lib.php';
 
-require "db.php";               //반드시 필요하다~!! (db.php 연결)
+// 게시물의 총 개수
+$total = 101;
+$limit = 10;
+$page_limit = 5;
+$page = (isset($_GET['page']) && $_GET['page'] != '' && is_numeric($_GET['page'])) ? $_GET['page'] : 1;
 
-if($_POST['mode'] == "input") {
+$data = range(1, $total);
 
-$subject = $_POST['subject'];
-$sql = "INSERT INTO `todolist`(subject) VALUES(:subject)";
-$stmt = $conn->prepare($sql);
-$stmt->bindParam(':subject', $subject);
-$rs = $stmt->execute();
+$start = ($page - 1) * $limit;
+$end = (($start + $limit) > $total) ? $total : ($start + $limit);
 
-// print_r($rs);
-
-if($rs) {
-    echo "
-    <script>
-    //alert('정상적으로 등록되었습니다.');
-    self.location.href='./index.php';
-    </script>
-    ";
-} else {
-    echo "
-    <script>
-    alert('등록 과정에서 오류가 발생하였습니다.');
-    history.go(-1);
-    </script>
-    ";
+// 게시물 출력
+for ($i = $start; $i < $end; $i++) {
+    if (isset($data[$i])) {
+        echo $data[$i] . '번 게시글 <br>';
+    }
 }
 
-//#취소선
-} else if($_POST['mode'] == 'done') {
-    $idx = $_POST['idx'];
-    do_undo($idx, 1);
-    // print_r($_POST);
- 
-    // $sql = "UPDATE `todolist` SET `status`=1 WHERE `idx`=:idx";
-    // $stmt = $conn->prepare($sql);
-    // $stmt->bindParam(':idx', $_POST['idx']);
-    // $stmt->execute();
-
-    // echo "
-    // <script>
-    //     self.location.href='./index.php';
-    // </script>
-    // ";
-}
-
-//#취소선을 취소
-else if($_POST['mode'] == 'undone') {
-    $idx = $_POST['idx'];
-    do_undo($idx, 0);
-    // print_r($_POST);
-
-    // $sql = "UPDATE `todolist` SET `status`=0 WHERE `idx`=:idx";
-    // $stmt = $conn->prepare($sql);
-    // $stmt->bindParam(':idx', $_POST['idx']);
-    // $stmt->execute();
-
-    // echo "
-    // <script>
-    //     self.location.href='./index.php';
-    // </script>
-    // ";
-}
-
-//#삭제 기능
-else if($_POST['mode'] == 'del') {
-    $sql = "DELETE FROM `todolist` WHERE `idx`=:idx";
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':idx', $_POST['idx']);
-    $stmt->execute();
-
-    echo "
-    <script>
-        self.location.href='./index.php';
-    </script>
-    
-    ";
-}  
-
-//#취소선,취소선을 취소 간단히(한번에)
-function do_undo($idx, $status) {
-    global $conn;               // global : 외부에 있는 변수를 쓰겠다~~
-
-    $sql = "UPDATE `todolist` SET `status`=:status WHERE `idx`=:idx";
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':idx',$idx);
-    $stmt->bindParam(':status', $status);
-    $stmt->execute();
-
-    echo "
-    <script>
-        self.location.href='./index.php';
-    </script>
-    
-    ";
-}
-
-
-
+// 페이지네이션 링크 출력
+echo my_pagination($total, $limit, $page_limit, $page, 'todolist_ok.php');
 
 ?>
