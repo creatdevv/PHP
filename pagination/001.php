ca2f3de99713c622116e78393e6b59521562e742
devlara <createdevlara@gmail.com>
2024-10-21 22:00:07 +0900

    html 구조 추가함

--- pagination/001.php
+++ pagination/001.php
@@ -12,15 +12,28 @@ $data = range(1, $total);
 
 $start = ($page - 1) * $limit;
 $end = (($start + $limit) > $total) ? $total : ($start + $limit);
+?>
 
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>페이지네이션 예시</title>
+</head>
+<body>
+
+<?php
 // 게시물 출력
 for($i = $start; $i < $end; $i++) {
-  if (isset($data[$i])) {
-    echo $data[$i] . '번 게시글 <br>';
-  }
+    if (isset($data[$i])) {
+        echo $data[$i] . '번 게시글 <br>';
+    }
 } 
 
 // 페이지네이션 링크 출력
 echo my_pagination($total, $limit, $page_limit, $page, '001.php');
-
 ?>
+
+</body>
+</html>
