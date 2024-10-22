4d9169f31a21eaa2b70dda536aa05587d4dc8392
devlara <createdevlara@gmail.com>
2024-10-22 22:23:07 +0900

    데이터 비밀번호 추가해서 접속하기

--- pagination/db.php
+++ pagination/db.php
@@ -1,16 +1,15 @@
 <?php 
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = ""; // 비밀번호 추가
 $dbname = "kingchobo";
 
 try {
-    $conn = new PDO("mysql:host=localhost;dbname=yourdbname", "username", "password");
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "DB connection successful!";
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
     exit();
 }
-
 ?>
