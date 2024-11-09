1c18ea60c8bfbd077ea8818ce1993d62db257cfe
devlara <createdevlara@gmail.com>
2024-11-09 13:56:35 +0900

    - HTML 구조 더 나은 구조로 제공함 - Prev/Next 링크 스타일(비활성화된 경우 스타일 적용시킴) - 페이지 이동 긱능(레이블 추가, 입력오류 방지)

--- pagination/lib.php
+++ pagination/lib.php
@@ -1,5 +1,5 @@
 <?php
-// PHP 오류 로그 확인
+// PHP 오류 로그 설정
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -7,54 +7,39 @@ error_reporting(E_ALL);
 function my_pagination($total, $limit, $page_limit, $page, $base_url) {
     $total_page = ceil($total / $limit);
     $start_page = ((floor(($page - 1) / $page_limit)) * $page_limit) + 1;
-    $end_page = $start_page + $page_limit - 1;
+    $end_page = min($start_page + $page_limit - 1, $total_page);
 
-    if ($end_page > $total_page) {
-        $end_page = $total_page;
-    }
-
-    $pagination_str = "";
+    $pagination_str = "<div class='pagination'>";
 
     // 현재 페이지 표시
-    $pagination_str .= "현재 페이지는 {$page}입니다. ";
+    $pagination_str .= "<p>현재 페이지: <strong>{$page}</strong> / 총 <strong>{$total_page}</strong> 페이지</p>";
 
     // First 링크
     if ($page > 1) {
         $pagination_str .= "<a href='{$base_url}?page=1'>First</a> ";
     }
 
-    // Prev 링크 (비활성화 포함)
-    $prev_page = $page - 1;
-    if ($prev_page < 1) {
-        $pagination_str .= "<span class='disabled'>Prev</span> ";
+    // Prev 링크
+    if ($page > 1) {
+        $pagination_str .= "<a href='{$base_url}?page=" . ($page - 1) . "'>Prev</a> ";
     } else {
-        $pagination_str .= "<a href='{$base_url}?page={$prev_page}'>Prev</a> ";
+        $pagination_str .= "<span class='disabled'>Prev</span> ";
     }
 
     // 페이지 번호 출력
     for ($i = $start_page; $i <= $end_page; $i++) {
         if ($i == $page) {
-            $pagination_str .= "<strong>{$i}</strong> ";
+            $pagination_str .= "<strong class='current'>{$i}</strong> ";
         } else {
             $pagination_str .= "<a href='{$base_url}?page={$i}'>{$i}</a> ";
-        } 
+        }
     }
 
-    // 몇 페이지씩 건너뛰기 기능
-    $jump_page = 5;
-    if ($page + $jump_page <= $total_page) {
-        $pagination_str .= "<a href='{$base_url}?page=" . ($page + $jump_page) . "'>+{$jump_page} Pages</a> ";
-    }
-    if ($page - $jump_page >= 1) {
-        $pagination_str .= "<a href='{$base_url}?page=" . ($page - $jump_page) . "'>-{$jump_page} Pages</a> ";
-    }
-
-    // Next 링크 (비활성화 포함)
-    $next_page = $page + 1;
-    if ($next_page > $total_page) {
-        $pagination_str .= "<span class='disabled'>Next</span> ";
+    // Next 링크
+    if ($page < $total_page) {
+        $pagination_str .= "<a href='{$base_url}?page=" . ($page + 1) . "'>Next</a> ";
     } else {
-        $pagination_str .= "<a href='{$base_url}?page={$next_page}'>Next</a> ";
+        $pagination_str .= "<span class='disabled'>Next</span> ";
     }
 
     // Last 링크
@@ -64,13 +49,14 @@ function my_pagination($total, $limit, $page_limit, $page, $base_url) {
 
     // 페이지 이동 입력창
     $pagination_str .= "
-        <form action='{$base_url}' method='GET'>
-            <input type='number' name='page' min='1' max='{$total_page}' value='{$page}' />
-            <input type='submit' value='Go' />
+        <form action='{$base_url}' method='GET' class='page-jump'>
+            <label for='page'>페이지 이동:</label>
+            <input type='number' name='page' min='1' max='{$total_page}' value='{$page}' required />
+            <button type='submit'>Go</button>
         </form>
     ";
 
+    $pagination_str .= "</div>";
     return $pagination_str;
 }
-
 ?>
