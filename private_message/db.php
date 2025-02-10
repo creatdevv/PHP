622991ae4f396718d6f93891775fbf3ebc7e8d01
devlara <createdevlara@gmail.com>
2025-02-10 23:15:44 +0900

    db table 생성

--- private_message/db.php
+++ private_message/db.php
@@ -7,3 +7,10 @@ CREATE TABLE IF NOT EXISTS private_messages (
     FOREIGN KEY (sender_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (receiver_id) REFERENCES users(id) ON DELETE CASCADE
 );
+
+
+<!-- #테이블 역할:
+sender_id: 보낸 사람 ID
+receiver_id: 받는 사람 ID
+message: 메시지 내용
+timestamp: 보낸 시간 -->
